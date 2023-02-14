cc3385cf08105a495565fbecead6dbffdc6f7608
belayhun-arage <belayhun24@gmail.com>
2023-02-14 14:56:54 +0300

    pinia state management

---
+++ src/stores/comments.js
@@ -0,0 +1,23 @@
+import {defineStore} from 'pinia'
+import  {usePostStore} from './posts'
+
+// define the comment store
+export default useCommentStore = defineStore({
+    id:'comment',
+    state: () => ({
+        comments:[],
+    }),
+    getters: {
+        getPostComments:()=>{
+            const postStore = usePostStore();
+            return state.comments.filter((post)=>post.postId=postStore.postId)
+        }
+    },
+    actions: {
+        async fetchComments(){
+            this.comments = await fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(response=>response.json())
+        },
+
+    }
+})
